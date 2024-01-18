25b4d3ab06d46ab45f3225fa12b6e911a70c99b7
Milyaz Kamil <milyazkamil@gmail.com>
2024-01-18 18:52:03 +0300

    Fetch and update movie data from OMDB API

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import {useEffect } from 'react';
+import Sidebar from './Sidebar';
+import MainContent from './MainContent';
+import { useGlobalContext } from './Context';
+
+const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
+const url = 'http://www.omdbapi.com/?t=';
+
+function App() {
+  const { setMovie, searchedMovie } = useGlobalContext();
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const resp = await fetch(`${url}${searchedMovie}&apikey=${API_KEY}`);
+        const data = await resp.json();
+        setMovie(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchMovie();
+  }, [searchedMovie]);
+  
+  return (
+    <>
+      <main id="main">
+        <Sidebar />
+        <MainContent />
+      </main>
+    </>
+  )
+}
+export default App;

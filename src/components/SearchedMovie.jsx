0386c2febccff0b6b7ac0b5769814844954a9893
Milyaz Kamil <milyazkamil@gmail.com>
2024-01-18 18:04:06 +0300

    Created a SearchedMovie component displaying an individual search movie.

---
+++ src/components/SearchedMovie.jsx
@@ -0,0 +1,11 @@
+import SearchedMovieCard from './SearchedMovieCard';
+
+const SearchedMovie = () => {
+  return (
+    <section id="searched-main-content">
+      <h1 id="searched-movie-title">Searched Movie</h1>
+      <SearchedMovieCard />
+    </section>
+  )
+}
+export default SearchedMovie;

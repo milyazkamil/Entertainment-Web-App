5ba580e1d6e7210213fa1631a56eeb290d644c88
Milyaz Kamil <milyazkamil@gmail.com>
2024-01-18 17:36:17 +0300

    Created TrendingMovies section with integrated Slider component.

---
+++ src/components/TrendingMovies.jsx
@@ -0,0 +1,13 @@
+import Slider from './Slider';
+
+const TrendingMovies = () => {
+  return (
+    <>
+      <section id="trending-movies">
+        <h2 id="trending-movies-title">Trending Movies</h2>
+        <Slider />
+      </section>
+    </>
+  )
+}
+export default TrendingMovies;
